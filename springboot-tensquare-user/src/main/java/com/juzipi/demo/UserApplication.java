323d7de0cb80ed5ea238f1f2a4639fb1ec55d937
Xie Jun <1124685815@qq.com>
2021-02-15 20:52:53 +0800

    解决注册和登录错误

--- springboot-tensquare-user/src/main/java/com/juzipi/demo/UserApplication.java
+++ springboot-tensquare-user/src/main/java/com/juzipi/demo/UserApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+
 @MapperScan("com.juzipi.demo.mapper")
 public class UserApplication {
     public static void main(String[] args) {
